cdbba7f7eaa9f9c8f9e205a0daafd8566164e626
123FAtangerang <149584464+123FAtangerang@users.noreply.github.com>
2025-04-25 16:56:28 +0700

    Add files via upload

---
+++ data/PerpipaanBukitTimah_19.js
@@ -0,0 +1,9 @@
+var json_PerpipaanBukitTimah_19 = {
+"type": "FeatureCollection",
+"name": "PerpipaanBukitTimah_19",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": 17.0, "folders": "Pipa Distribusi", "DMA": null, "Diameter": "315 mm", "Jenis": "HDPE", "Jenis Jari": "Distribusi Utama", "Keadaan": "Eksisting", "Kedalaman": "1.5 m", "Sumber Dan": "NUWSP", "Th. Pasang": null, "Zona": null, "id": "18", "Panjang": 117.371 }, "geometry": { "type": "LineString", "coordinates": [ [ 101.337308643797684, 1.620094267082586 ], [ 101.337308518062656, 1.619984615963427 ], [ 101.337341726387592, 1.619960170277721 ], [ 101.337462796828092, 1.619944752635857 ], [ 101.337729208812888, 1.619929256594605 ], [ 101.337830781554132, 1.619935834357056 ], [ 101.337827602385019, 1.620000047246851 ], [ 101.337808238793329, 1.620351835422885 ] ] } },
+{ "type": "Feature", "properties": { "fid": 32.0, "folders": "Pipa Distribusi", "DMA": null, "Diameter": "250 mm", "Jenis": "HDPE", "Jenis Jari": "Distribusi Utama", "Keadaan": "Eksisting", "Kedalaman": null, "Sumber Dan": "APBD", "Th. Pasang": null, "Zona": null, "id": "34", "Panjang": 0.0 }, "geometry": { "type": "LineString", "coordinates": [ [ 101.434583914822142, 1.670717718851003 ], [ 101.434583914822142, 1.670717718851003 ] ] } }
+]
+}
---
+++ data/PerpipaanLaksamana_13.js
@@ -0,0 +1,9 @@
+var json_PerpipaanLaksamana_13 = {
+"type": "FeatureCollection",
+"name": "PerpipaanLaksamana_13",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": 32.0, "folders": "Pipa Distribusi", "DMA": null, "Diameter": "250 mm", "Jenis": "HDPE", "Jenis Jari": "Distribusi Utama", "Keadaan": "Eksisting", "Kedalaman": null, "Sumber Dan": "APBD", "Th. Pasang": null, "Zona": null, "id": "34", "Panjang": 0.0 }, "geometry": { "type": "LineString", "coordinates": [ [ 101.434583914822142, 1.670717718851003 ], [ 101.434583914822142, 1.670717718851003 ] ] } },
+{ "type": "Feature", "properties": { "fid": 1872.0, "folders": "Pipa Distribusi", "DMA": null, "Diameter": "63 mm", "Jenis": "HDPE", "Jenis Jari": "Distribusi Tersier", "Keadaan": "Eksisting", "Kedalaman": null, "Sumber Dan": null, "Th. Pasang": "2024", "Zona": null, "id": null, "Panjang": 335.672 }, "geometry": { "type": "LineString", "coordinates": [ [ 101.439972732102248, 1.679777627389595 ], [ 101.436956196321376, 1.679747647173422 ] ] } }
+]
+}
