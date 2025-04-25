94fa4adcaca93362c5224c18aac002823447d0ec
123FAtangerang <149584464+123FAtangerang@users.noreply.github.com>
2025-04-25 17:08:26 +0700

    Add files via upload

---
+++ data/PerpipaanBaganKeladi_21.js
@@ -0,0 +1,8 @@
+var json_PerpipaanBaganKeladi_21 = {
+"type": "FeatureCollection",
+"name": "PerpipaanBaganKeladi_21",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": 32.0, "folders": "Pipa Distribusi", "DMA": null, "Diameter": "250 mm", "Jenis": "HDPE", "Jenis Jari": "Distribusi Utama", "Keadaan": "Eksisting", "Kedalaman": null, "Sumber Dan": "APBD", "Th. Pasang": null, "Zona": null, "id": "34", "Panjang": 0.0 }, "geometry": { "type": "LineString", "coordinates": [ [ 101.434583914822142, 1.670717718851003 ], [ 101.434583914822142, 1.670717718851003 ] ] } }
+]
+}
